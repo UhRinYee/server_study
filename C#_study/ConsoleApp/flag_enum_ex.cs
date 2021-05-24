21afba135bd49c1d55d18e316ccd9adc47aa1b6d
UhRinYee <yoonsu2435@naver.com>
2021-05-24 14:51:09 +0900

    struct_ex

---
+++ C#_study/ConsoleApp/flag_enum_ex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class flag_enum_ex
+    {
+        [Flags]
+        enum Border
+        {
+            None = 0,
+            Top = 1,        // 0000 0001
+            Right = 2,
+            Bottom = 4,     // 0000 0011
+            Left = 8
+        }
+        static void Main(String[] args)
+        {
+            // OR 연산자로 다중 플래그 할당
+            Border b = Border.Top | Border.Bottom;
+
+            // & 연산자로 플래그 체크
+            if((b & Border.Top) != 0)                   // b에 Border.Top이 없음
+            {
+                // HasFlag()이용 플래그 체크
+                if (b.HasFlag(Border.Bottom))           // 
+                {
+                    // "Top, Bottom"출력
+                    Console.WriteLine(b.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ C#_study/ConsoleApp/struct_ex.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class struct_ex
+    {
+        struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Width;
+            public int Height;
+            public Rect(int left, int top, int width, int height)       // 구조체에 생성자가 선언이 된다... 개 혁명;
+            {
+                Left = left;
+                Top = top;
+                Width = width;
+                Height = height;
+            }
+        }
+
+        static void Main(String[] args)
+        {
+            Rect r = new Rect();
+            r.Left = 10;
+            r.Top = 10;
+            r.Width = 10;
+            r.Height = 10;
+
+            Draw(r);
+        }
+
+        static void Draw(Rect r)
+        {
+            DrawRectangle(r);
+            PrintData(r);
+        }
+
+        static void DrawRectangle(Rect r)
+        {
+            //.... 생략 ....
+        }
+        static void PrintData(Rect r)
+        {
+            int right = r.Left + r.Width;
+            int bottom = r.Top + r.Height;
+            Console.WriteLine($"{r.Left},{r.Top} ~ {right},{bottom}");
+        }
+    }
+}
