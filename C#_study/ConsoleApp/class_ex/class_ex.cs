417ac4e977689eb39c4cae69ed2ba4326b648be7
UhRinYee <yoonsu2435@naver.com>
2021-05-25 15:49:42 +0900

    class_ex

---
+++ C#_study/ConsoleApp/class_ex/class_ex.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    class class_ex
+    {
+        // 필드
+        private int width;
+        private int height;
+        private int depth;
+        private Color color;        // System.Drawing
+
+        // 생성자 (Constructor)
+        // private : class 내에서만 사용 가능
+        // public : 접근제한자 외부에서 member 엑세스 가능
+        public class_ex()
+        {
+            width = 10;
+            height = 10;
+            depth = 5;
+            color = Color.Red;
+        }
+
+        public class_ex(int width, int height, int depth, Color color)
+        {
+            this.width = width;                     //this : class의 member 참조
+            this.height = height;
+            this.depth = depth;
+            this.color = color;
+        }
+
+        // 속성 (property)
+        public int Width
+        {
+            get { return this.width; }      // 읽기 전용
+            set {
+                if (value > 0)
+                {
+                    this.width = value;
+                }
+            }
+        }
+        public Color Color
+        {
+            get { return this.color; }
+        }
+
+        public int Volume
+        {
+            get { return width * height * depth; }
+        }
+
+        // 메서드 (Method)
+        public void MakeBrick()
+        {
+            //Event Fire
+            if (ProcessStarted != null)
+            {
+                ProcessStarted(this, EventArgs.Empty);
+            }
+            Step1();
+            Step2();
+            Step3();
+
+            if (ProcessCompleted != null)
+            {
+                ProcessCompleted(this, EventArgs.Empty);
+            }
+        }
+
+        private void Step1()
+        {
+            Console.WriteLine("# Step1 #");
+        }
+        private void Step2()
+        {
+            Console.WriteLine("# Step2 #");
+        }
+        private void Step3()
+        {
+            Console.WriteLine("# Step3 #");
+        }
+
+        // 이벤트
+        public event EventHandler ProcessStarted;
+        public event EventHandler ProcessCompleted;
+    }
+}
---
+++ C#_study/ConsoleApp/class_ex/class_ex_main_program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    class class_ex_main_program
+    {
+        static void Main(String[] args)
+        {
+            class_ex brick1 = new class_ex();            // class_ex 객체를 brick에 할당
+            class_ex brick2 = new class_ex(50, 100, 20, Color.Gray);
+
+            int w = brick2.Width;
+            brick2.Width = 100;                 // get을 사용하면 읽기전용이므로 실행 불가.
+                                                // set도 사용
+            int w2 = brick2.Width;
+            Color c = brick2.Color;             
+
+            Console.WriteLine(w);
+            Console.WriteLine(w2);
+            Console.WriteLine(c);
+
+            int v = brick2.Volume;
+
+            Console.WriteLine(v);
+
+            brick2.ProcessStarted += Brick2_ProcessStarted;
+            brick2.ProcessCompleted += Brick2_ProcessCompleted;
+            brick2.MakeBrick();
+
+        }
+
+        private static void Brick2_ProcessStarted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Process Started");
+        }
+
+        private static void Brick2_ProcessCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Process Completed");
+        }
+    }
+}
